7c12fabf2eb07b22882e13f1acf927134507a5e3
T1mono <167001770+T1mono@users.noreply.github.com>
2025-09-03 15:02:54 +0500

    Added ProcessedMessageResponseDto

--- service_c/src/main/java/mentorship/roadmap/microservices/service_c/dto/ProcessedMessageResponseDto.java
+++ service_c/src/main/java/mentorship/roadmap/microservices/service_c/dto/ProcessedMessageResponseDto.java
@@ -2,6 +2,12 @@ package mentorship.roadmap.microservices.service_c.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class ProcessedMessageResponseDto {
+    private Long id;
+    private String message;
+    private String type;
+    private LocalDateTime processedAt;
 }
