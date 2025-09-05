86809d55fa1ad788eabab17ef42e0e82bc8ac104
T1mono <167001770+T1mono@users.noreply.github.com>
2025-09-05 16:13:40 +0500

    Добавил KafkaConsumerConfig

---
+++ service_a/src/main/java/mentorship/roadmap/microservices/consumer/KafkaConsumerConfig.java
@@ -0,0 +1,32 @@
+package mentorship.roadmap.microservices.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "service_a_group"); // Идентификатор группы потребителей - все инстансы сервиса будут в одной группе
+        configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // "earliest" - читать с самого начала (не терять сообщения)
+
+        return new DefaultKafkaConsumerFactory<>(configProps);
+    }
+}
