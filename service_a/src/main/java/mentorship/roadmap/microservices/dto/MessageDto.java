f5fa0ae9eeb7f288414cb9301ea6873b7f31f855
T1mono <167001770+T1mono@users.noreply.github.com>
2025-09-05 14:16:08 +0500

    Добавил сериализацию в DTO

--- service_a/src/main/java/mentorship/roadmap/microservices/dto/MessageDto.java
+++ service_a/src/main/java/mentorship/roadmap/microservices/dto/MessageDto.java
@@ -4,14 +4,20 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
-public class MessageDto {
+public class MessageDto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @NotBlank(message = "Message cannot be blank")
-    @Size(min = 1, max = 1000, message = "Message must be 1 and 1000 character")
+    @Size(min = 1, max = 1000, message = "Message must be between 1 and 1000 character")
     private String message;
 
     @NotBlank(message = "Type cannot be blank")
-    @Size(min = 1, max = 1000, message = "Message must be 1 and 1000 character")
+    @Size(min = 1, max = 1000, message = "Message must be between 1 and 1000 character")
     private String type;
 }
